7d7d4b27017ea53e04d8946c2315294b5940b7b8
massa <massafranck@gmail.com>
2018-08-01 22:01:04 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php
-//Déclaration des variables pour les trois nombres
-$number1 = 10;
-$number2 = 47;
-$number3 = 10;
+//fonction calcul avec les parametre par défaut
+  function calculate($number1 = 125, $number2 = 25, $number3 = 364){
+    $result = $number1 + $number2 + $number3;
+    return $result;
+  }
 ?>
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice8</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Déclaration de la fonction somme et retourne la valeur
-    function sum($number1, $number2, $number3){
-      return $number1 + $number2 + $number3;
-    }
-    //Affichage du réultat de l'addition
-    echo sum($number1, $number2, $number3);
-    ?>
-  </p>
-</body>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <p>
+
+<?= calculate(10,10,10); //Affichage avec de nouvelles variables ?>
+    </p>
+  </body>
 </html>
